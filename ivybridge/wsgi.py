fd53e1256ea2f7afddbc26777ef26abb5f42033c
MrToyy <jason.wutong@gmail.com>
2017-06-26 21:25:04 +0800

    http update

--- ivybridge/wsgi.py
+++ ivybridge/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import sys
+#import sys
 
-sys.path.append('/opt/bitnami/apps/django/django_projects/IvyBridge')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/IvyBridge/egg_cache")
+#sys.path.append('/opt/bitnami/apps/django/django_projects/IvyBridge')
+#os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/IvyBridge/egg_cache")
 
 from django.core.wsgi import get_wsgi_application
 
